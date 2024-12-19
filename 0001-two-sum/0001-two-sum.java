fffb7c170339ddb888b61ceb0bef876899a9f7dc
OnePathToFreedom <kalmykovmikhail.s@gmail.com>
2024-12-19 20:59:03 +0100

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
-            int requiredNum = target - nums[i];
+        for(int i = 0; i < nums.length; i++) {                            //it was first try
+            int requiredNum = target - nums[i];                           //this condition made from 32 ms -> 84 ms
             for(int j = i + 1; j < nums.length; j++) {                    //for(int j = 0; j < nums.length; j++) {
                 if(nums[j] == requiredNum) return new int[]{i, j};             //if(nums[j] == requiredNum && i != j) return new int[]{i, j};
-            }                                                                    // this condition made from 32 ms -> 84 ms
+            }                                                             
         }
         return new int[]{-1,-1};
     }

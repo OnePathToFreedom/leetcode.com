9db8ced736a85193de8e649f012427de4722266a
OnePathToFreedom <kalmykovmikhail.s@gmail.com>
2024-12-20 19:34:12 +0100

    Time: 5 ms (72.51%), Space: 43.8 MB (73.21%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int number = x;
-        int reversedNumber = 0; 
-        while(number > 0) {
-            reversedNumber = reversedNumber * 10 + number % 10;
-            number = number / 10;
+        int original_number = x;
+        int reversed_number = 0;
+        while(original_number > 0) {
+            reversed_number = reversed_number * 10 + original_number % 10;
+            original_number /= 10;
         }
-        if(reversedNumber == x) return true;
-        return false;
+        return reversed_number == x;
     }
 }

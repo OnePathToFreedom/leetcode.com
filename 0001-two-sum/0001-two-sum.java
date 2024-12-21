5d221bbb0ad4e9c924dcacfa665ef46fbd320363
OnePathToFreedom <kalmykovmikhail.s@gmail.com>
2024-12-22 00:27:32 +0100

    Time: 32 ms (49.57%), Space: 45 MB (30.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    for(int i = 0; i < nums.length; i++) {
-        int number = target - nums[i];
-        for(int j = i + 1; j < nums.length; j++) {
-            if(nums[j] == number) {
-                return new int[] {i, j};
+        for(int i = 0; i < nums.length; i++) {
+            int required_number = target - nums[i];
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[j] == required_number) return new int[] {i, j};
             }
-        }
-    }
-    return new int[] {};
+        } 
+        return new int[] {};
     }
 }
